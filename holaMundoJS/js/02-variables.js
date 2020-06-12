fb0f1eaeab3d3278e4e981c1f5d24f988d86392d
Enrique <enrique.navarro@ingenieros.com>
2020-06-12 23:33:03 +0200

    Variables primera parte

---
+++ holaMundoJS/js/02-variables.js
@@ -0,0 +1,13 @@
+//VARIABLES
+//Es un contenedor de informacion
+
+var pais = "Colombia";
+var continente = "America";
+var antiguedad = 2020;
+var pais_y_continente = pais + ' ' + continente;
+
+pais = "mexico";
+continente = "latinoamerica";
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
