866a08e1faa1b223aea3733dc08ed2bd0b5036bc
Enrique <enrique.navarro@ingenieros.com>
2020-06-13 13:36:33 +0200

    diferencias var y let

--- holaMundoJS/js/02-variables.js
+++ holaMundoJS/js/02-variables.js
@@ -5,6 +5,10 @@ var pais = "Colombia";
 var continente = "America";
 var antiguedad = 2020;
 var pais_y_continente = pais + ' ' + continente;
+let prueba;
+
+prueba = "hola";
+alert(prueba);
 
 pais = "mexico";
 continente = "latinoamerica";
---
+++ holaMundoJS/js/03-letyvar.js
@@ -0,0 +1,25 @@
+'use strict'
+
+//Pruebas Let y Var
+
+//Prueba var
+var numero = 40;
+console.log(numero);
+
+if (true) {
+    var numero = 50;
+    console.log(numero);
+}
+
+console.log(numero);
+
+//Prueba let
+var texto = "Curso JS";
+console.log(texto);
+
+if (true) {
+    let texto = "Curo LAravel";
+    console.log(texto);
+}
+
+console.log(texto);
