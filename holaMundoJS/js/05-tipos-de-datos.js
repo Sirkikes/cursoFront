6a5be6b5a17331af10ff42a88e4d9eb8b144352c
Enrique <enrique.navarro@ingenieros.com>
2020-07-19 16:14:25 +0200

    tipos de datos

---
+++ holaMundoJS/js/05-tipos-de-datos.js
@@ -0,0 +1,18 @@
+'use strict'
+
+//Operadores
+var numero1 = 7;
+var numero2 = 13;
+var operacion = numero1 + numero2;
+
+alert("El resultado de la operacion es: " + operacion);
+
+//Tipos de Datos
+var numero_entero = 33;
+var cadena_texto = "Soy una cadena de texto";
+var verdadero_falso = true;
+
+var numero_falso = "33.4";
+
+
+console.log(typeof numero_entero);
